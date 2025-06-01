9043f1cc6ac23c13e91fcdf55d1b671a32995c69
jake <jakeoleary96@gmail.com>
2025-06-01 13:42:53 -0700

    building out BingoTiles, styling

--- src/BoardTile.tsx
+++ src/BoardTile.tsx
@@ -1,7 +1,22 @@
+import type React from "react"
+
+
 export const BoardTile: React.FC<object> = () => {
   return (
-    <div className={"w-square h-square flex items-center justify-center border border-black p-6"}>
-      <p>boardtile</p>
+    <div className={"w-square h-square flex items-center justify-center border border-black"}>
+      <ul className="flex flex-col items-stretch justify-center w-full h-full">
+        <TileGoalButton goalText={"Goal Tile 1"} />
+        <div className="h-px w-full bg-gradient-to-r from-transparent via-slate-400 to-transparent" />
+        <TileGoalButton goalText={"Goal Tile 2"} />
+        <div className="h-px w-full bg-gradient-to-r from-transparent via-slate-400 to-transparent" />
+        <TileGoalButton goalText={"Goal Tile 3"} />
+      </ul>
     </div>
+  )
+}
+
+const TileGoalButton: React.FC<{goalText: string}> = ({ goalText }) => {
+  return (
+    <button className="w-full px-6 py-2"> { goalText } </button>
   )
 }
